cc39b736939815c23aba520cb89febb0f170efcf
Andrew Davison <andrew.davison@unic.cnrs-gif.fr>
2015-11-19 16:53:05 +0100

    Declare data variables

--- xkcd.py
+++ xkcd.py
@@ -9,16 +9,20 @@ with plt.xkcd():
     # Based on "The Data So Far" from XKCD by Randall Monroe
     # http://xkcd.com/373/
 
+    index = [0, 1]
+    data = [0, 100]
+    labels = ['CONFIRMED BY EXPERIMENT', 'REFUTED BY EXPERIMENT']
+
     fig = plt.figure()
     ax = fig.add_axes((0.1, 0.2, 0.8, 0.7))
-    ax.bar([-0.125, 1.0 - 0.125], [0, 100], 0.25)
+    ax.bar(index, data, 0.25)
     ax.spines['right'].set_color('none')
     ax.spines['top'].set_color('none')
     ax.xaxis.set_ticks_position('bottom')
     ax.set_xticks([0, 1])
     ax.set_xlim([-0.5, 1.5])
     ax.set_ylim([0, 110])
-    ax.set_xticklabels(['CONFIRMED BY EXPERIMENT', 'REFUTED BY EXPERIMENT'])
+    ax.set_xticklabels(labels)
     plt.yticks([])
 
     plt.title("CLAIMS OF SUPERNATURAL POWERS")
